969790e21c4a57aa26fccf68588a2857073ebce9
Adin Ackerman <adinackerman@gmail.com>
2023-06-16 12:57:37 -0700

    Initial Commit

---
+++ AccessAbility/AccessAbilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessAbilityApp.swift
+//  AccessAbility
+//
+//  Created by Adin Ackerman on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessAbilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
